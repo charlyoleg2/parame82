6f0f6154fe3f55133ff26e977471ed5974f3ed98
charles <charles.braquet@e.email>
2024-03-27 21:09:57 +0100

    more faces for the stopDoor

--- desi82/src/printer3d/doorstop.ts
+++ desi82/src/printer3d/doorstop.ts
@@ -13,8 +13,10 @@ import type {
 	//tSubDesign
 } from 'geometrix';
 import {
+	//point,
 	contour,
 	contourCircle,
+	ctrRectangle,
 	figure,
 	//degToRad,
 	radToDeg,
@@ -22,6 +24,7 @@ import {
 	pNumber,
 	//pCheckbox,
 	//pDropdown,
+	pSectionSeparator,
 	initGeom,
 	EExtrude,
 	EBVolume
@@ -34,9 +37,10 @@ const pDef: tParamDef = {
 		//pNumber(name, unit, init, min, max, step)
 		pNumber('H1', 'mm', 40, 5, 200, 1),
 		pNumber('L1', 'mm', 100, 10, 300, 1),
+		pNumber('W1', 'mm', 30, 5, 200, 1),
+		pSectionSeparator('Details'),
 		pNumber('R1', 'mm', 2, 1, 30, 1),
-		pNumber('E1', 'mm', 4, 1, 20, 1),
-		pNumber('W1', 'mm', 30, 5, 200, 1)
+		pNumber('E1', 'mm', 4, 1, 20, 1)
 	],
 	paramSvg: {
 		H1: 'doorstop_profile.svg',
@@ -59,6 +63,8 @@ const minRadius = 2;
 function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const rGeome = initGeom(pDef.partName + suffix);
 	const figProfile = figure();
+	const figTop = figure();
+	const figSide = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
 		// step-4 : some preparation calculation
@@ -84,6 +90,8 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			.closeSegStroke();
 		figProfile.addMain(ctrDoorstop);
 		figProfile.addMain(contourCircle(param.H1 / 2, param.H1 / 2, param.H1 / 2 - param.E1));
+		const listPosX: number[] = [];
+		const listRadius: number[] = [];
 		let x1 = param.L1;
 		let y1 = Math.tan(a2) * x1;
 		let radius = y1 - param.E1;
@@ -91,14 +99,30 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			x1 = x1 - radius;
 			y1 = Math.tan(a2) * x1;
 			radius = y1 - param.E1;
-			figProfile.addMain(contourCircle(Ltot - x1, y1, radius));
+			const tPosX = Ltot - x1;
+			figProfile.addMain(contourCircle(tPosX, y1, radius));
+			listPosX.push(tPosX);
+			listRadius.push(radius);
 			x1 = x1 - radius - param.E1;
 			y1 = Math.tan(a2) * x1;
 			radius = y1 - param.E1;
 		}
+		// figTop
+		figTop.addMain(ctrRectangle(0, 0, param.H1, param.W1));
+		figTop.addMain(ctrRectangle(param.H1, 0, param.L1, param.W1));
+		figTop.addSecond(ctrRectangle(param.E1, 0, param.H1 - 2 * param.E1, param.W1));
+		for (const [idx, px] of listPosX.entries()) {
+			const tR = listRadius[idx];
+			figTop.addSecond(ctrRectangle(px - tR, 0, 2 * tR, param.W1));
+		}
+		// figSide
+		figSide.addMain(ctrRectangle(0, 0, param.W1, param.H1));
+		figSide.addSecond(ctrRectangle(0, param.E1, param.W1, param.H1 - 2 * param.E1));
 		// final figure list
 		rGeome.fig = {
-			profile: figProfile
+			faceProfile: figProfile,
+			faceTop: figTop,
+			faceSide: figSide
 		};
 		// step-8 : recipes of the 3D construction
 		const designName = rGeome.partName;
@@ -106,7 +130,7 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			extrudes: [
 				{
 					outName: `subpax_${designName}`,
-					face: `${designName}_profile`,
+					face: `${designName}_faceProfile`,
 					extrudeMethod: EExtrude.eLinearOrtho,
 					length: param.W1,
 					rotate: [0, 0, 0],
