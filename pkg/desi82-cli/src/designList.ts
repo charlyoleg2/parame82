81a60b8ae5d2964e0df85742f104b02f63046195
charlyoleg <charles.braquet@gmx.fr>
2024-09-16 01:55:22 +0200

    fix design name in designList

--- pkg/desi82-cli/src/designList.ts
+++ pkg/desi82-cli/src/designList.ts
@@ -5,7 +5,7 @@ import { doorstopDef, pencilHolderDef, spiralDef } from 'desi82';
 
 const designList: tAllPageDef = {
 	'desi82/doorstop': doorstopDef,
-	'desi82/pencilHolder': pencilHolderDef,
+	'desi82/pencil_holder': pencilHolderDef,
 	'desi82/spiral': spiralDef
 };
 
